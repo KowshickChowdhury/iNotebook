d8d7187a9d470733ec05483ab5fe5c803ab5c4a3
KowshickChowdhury <kowshick1170@gmail.com>
2023-09-21 19:55:36 +0600

    Start a new inotebook project

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const connectToMongo = ()=>{
+    // mongoose.connect(mongoURI, ()=>{
+    //     console.log("Connected to Mongo Succefully");
+    // })
+        mongoose.connect('mongodb://localhost:27017/mydatabase', { useNewUrlParser: true })
+        .then(() => {
+            console.log('MongoDB connected successfully');
+        })
+        .catch((err) => {
+            console.error('MongoDB connection error: ' + err);
+    });
+}
+module.exports = connectToMongo;
